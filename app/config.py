34a75ed0d9087fc3f013e24817b8e2fc7d77f002
TheDoctorZoose <147032259+TheDoctorZoose@users.noreply.github.com>
2025-05-16 21:08:43 +0800

    Fix formatting of load_dotenv path in config.py for consistency
    Signed-off-by: TheDoctorZoose <147032259+TheDoctorZoose@users.noreply.github.com>

--- app/config.py
+++ app/config.py
@@ -3,7 +3,8 @@ import os
 from dotenv import load_dotenv
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-load_dotenv(os.path.join(basedir, '..', '.env'))
+load_dotenv(os.path.join(basedir, "..", ".env"))
+
 
 class Config:
     SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "app.db")
