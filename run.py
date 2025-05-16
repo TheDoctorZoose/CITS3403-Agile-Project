747d4c094b401889a7d08e8e865ef6b372aa02fa
MasterLu0309 <167738226+MasterLu0309@users.noreply.github.com>
2025-05-16 21:51:22 +0800

    Update run.py
    Signed-off-by: MasterLu0309 <167738226+MasterLu0309@users.noreply.github.com>

--- run.py
+++ run.py
@@ -1,35 +1,29 @@
-import os
+from app import create_app, db
+from flask_migrate import upgrade, migrate, init
 import threading
 import time
-
-from flask_migrate import upgrade, migrate, init
-
-from app import create_app
+import os
+from tests.selenium_tests import selenium_test  
 
 app = create_app()
 
-
 def run_selenium_tests():
-    time.sleep(3)
-    run_selenium_tests()
-
+    time.sleep(3)  
+    selenium_test.run_selenium_tests()  
 
 def run_migrations():
     with app.app_context():
         try:
-            # Create migrations folder if not exists
             if not os.path.exists("migrations"):
                 init()
             migrate(message="Auto migration")
         except Exception as e:
-            print(f"[Migration warning] {e}")  # Ignore "No changes detected"
+            print(f"[Migration warning] {e}")
         upgrade()
 
-
 if __name__ == "__main__":
-
     if os.environ.get("WERKZEUG_RUN_MAIN") == "true":
-        run_migrations()  # Run migrations before server starts
+        run_migrations()
         threading.Thread(target=run_selenium_tests).start()
 
-    app.run(debug=True)
+    app.run(debug=True, use_reloader=False)
