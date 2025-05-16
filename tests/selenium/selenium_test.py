4e801ede77a8696b52e37f6cb9f7a0783edb4509
TheDoctorZoose <147032259+TheDoctorZoose@users.noreply.github.com>
2025-05-16 19:24:06 +0800

    Add missing import for time module in selenium_test.py
    Signed-off-by: TheDoctorZoose <147032259+TheDoctorZoose@users.noreply.github.com>

--- tests/selenium/selenium_test.py
+++ tests/selenium/selenium_test.py
@@ -1,8 +1,10 @@
+import time
 import unittest
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-import time
+
 
 class FlaskSeleniumTest(unittest.TestCase):
 
