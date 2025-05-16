2ea06496d4f4d260998983f5ca413f6f165df345
MasterLu0309 <167738226+MasterLu0309@users.noreply.github.com>
2025-05-16 15:54:59 +0800

    Add files via upload
    Signed-off-by: MasterLu0309 <167738226+MasterLu0309@users.noreply.github.com>

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
-from app import create_app
+from app import create_app, db
+from flask_migrate import upgrade, migrate, init
 import threading
 import time
 import os
@@ -10,9 +11,20 @@ def run_selenium_tests():
     import app.selenium_test
     app.selenium_test.run_selenium_tests()
 
+def run_migrations():
+    with app.app_context():
+        try:
+            # Create migrations folder if not exists
+            if not os.path.exists("migrations"):
+                init()
+            migrate(message="Auto migration")
+        except Exception as e:
+            print(f"[Migration warning] {e}")  # Ignore "No changes detected"
+        upgrade()
+
 if __name__ == "__main__":
-   
     if os.environ.get("WERKZEUG_RUN_MAIN") == "true":
+        run_migrations()  # Run migrations before server starts
         threading.Thread(target=run_selenium_tests).start()
-    
+
     app.run(debug=True)
